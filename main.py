cd2a669a2aa2ca8a005f050c3e17477460f00921
Bindi Nagda <nagdabindi@gmail.com>
2023-09-26 18:27:37 -0400

    Using Sec 3.1 of paper.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 
 # B-splines of degree n
@@ -16,15 +17,15 @@ def B(x, n):
 
 # x-component of vector field U_2a (note: U_2a is discretely divergence free)
 def x_vel(x,y):
-   u = np.sin(x+2)*np.sin(y+4)   
+   u = np.sin(370*x+2)*np.sin(370*y+4)   
    return u
 
 # y-component of vector field U_2a
 def y_vel(x,y):
-   v = np.cos(x+2)*np.cos(y+4)
+   v = np.cos(370*x+2)*np.cos(370*y+4)
    return v
 
-# Equation 9 in the paper
+# Equation 9 in the paper. Note: dx and dy are input grid cell sizes
 def u_interp(x, y, dx, dy):
    xx = np.arange(0,2+dx,dx)/2
    yy = np.arange(0,2+dy,dy)/2
@@ -37,7 +38,7 @@ def u_interp(x, y, dx, dy):
          u = u + x_vel(x_sides[i+1], y_centers[j])*B((x - x_sides[i+1])/dx, 3)*B((y - y_centers[j])/dy, 2)
    return u
 
-# Equation 10 in the paper      
+# Equation 10 in the paper. Note: dx and dy are input grid cell sizes      
 def v_interp(x, y, dx, dy):
    xx = np.arange(0,2+dx,dx)/2
    yy = np.arange(0,2+dy,dy)/2
@@ -49,6 +50,21 @@ def v_interp(x, y, dx, dy):
       for j in range(len(y_centers)):
          v = v + y_vel(x_centers[i], y_sides[j+1])*B((x - x_centers[i])/dx, 2)*B((y - y_sides[j+1])/dy, 3)
    return v
+
+# Equation 17 in the paper
+def div_17(x, y, dx, dy): 
+   xx = np.arange(0,2+dx,dx)/2
+   yy = np.arange(0,2+dy,dy)/2
+   x_centers = xx[1::2] 
+   y_centers = yy[1::2]
+   y_sides = yy[::2]
+   d = 0.0
+   for i in range(len(x_centers)):
+      for j in range(len(y_centers)):
+         d = (x_vel(x_sides[i+1], y_centers[j]) - x_vel(x_sides[i], y_centers[j]))/dx \
+            + (y_vel(x_centers[i], y_sides[j+1]) - y_vel(x_centers[i], y_sides[j]))/dy
+         d = d*B((x - x_centers[i])/dx, 2)*B((y - y_centers[j])/dy, 2)
+   return d
 
 if __name__=="__main__":
 
@@ -63,8 +79,8 @@ if __name__=="__main__":
    ax.grid(True)
    plt.savefig('bsplines.png')
 
-   dx = 1/32
-   dy = 1/32
+   dx = np.double(1/64) # input grid
+   dy = np.double(1/64) # input grid
    xx = np.arange(0,2+dx,dx)/2
    yy = np.arange(0,2+dy,dy)/2
    x_sides = xx[::2]    # cell-sides 
@@ -86,14 +102,49 @@ if __name__=="__main__":
       return d
 
    # Randomly sample the computational domain to generate test coordinates
-   x_coords =  np.random.uniform(low=0.1, high=0.9, size=(50,))   # Points near the domain boundary result in non-zero divergence
-   y_coords =  np.random.uniform(low=0.1, high=0.9, size=(50,))
-   tolerance = 1e-9
+   x_coords =  np.random.uniform(low=2*dx, high=1.0-2*dx, size=(64,))  
+   y_coords =  np.random.uniform(low=2*dx, high=1.0-2*dx, size=(64,))
+   tolerance = 1e-8
    delta = 1e-6
+   x_coords = []
+   y_coords = []
+   # Choose internal points on our refined 128 x 128 grid
+   for i in np.arange(21,100): 
+      x_coords.append(i/128)   # Any coordinates that fall on the input grid result in a non-zero divergence. Why? Surface plot below provides a little insight.
+      y_coords.append(i/128)
    for x in x_coords:
       for y in y_coords:  
          if div(x,y,delta,dx,dy) > tolerance:
             print(f"Divergence non-zero at {(x,y)}. Divergence = {div(x,y,delta,dx,dy)}")
-         # else:
-         #    print(f"Divergence-free at cell {(x,y)}. Divergence = {div(x,y,delta,dx,dy)}")
+         else:
+            print(f"Divergence-free at cell {(x,y)}. Divergence = {div(x,y,delta,dx,dy)}")
+  
+   # Surface plots of original velocity field and interpolated velocity fields.
+   dx = 1/8 # input grid
+   dy = 1/8 # input grid
+   xx = np.arange(0,1,1/128)
+   yy = np.arange(0,1,1/128)
+   zz = []
+
+   for x in xx:
+      for y in yy:
+         #zz.append(x_vel(x,y))
+         zz.append(u_interp(x,y,dx,dy))
+
+   fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+   # Make data.
+   X, Y = np.meshgrid(xx, yy)
+   Z = np.array(zz)
+   Z = np.reshape(zz,[xx.shape[0],yy.shape[0]])
+
+   # Plot the surface.
+   surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                        linewidth=0, antialiased=False)
+
+   # Add a color bar which maps values to colors.
+   fig.colorbar(surf)
+
+   plt.savefig('u_interpolated.png')
+   
 
